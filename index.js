e418274f7ae8e154138052fadf69eeb01fca14df
Jamila <jamnjeri@gmail.com>
2022-11-23 16:06:37 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+let multiply;
+let random;
+let mod;
+let max;
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 34;
+let num4 = 5;
+
+//Equation whose result = 62
+multiply = num1 * num2;
+
+//Generates random number > 0
+random = (Math.ceil(Math.random() * 10 + 1)); 
+
+// Calculates a remainder = 4
+mod = num3 % num4;
+
+//Returns 20 as the highest number in the set
+max = Math.max(4, 12, 20, 14, 5);
